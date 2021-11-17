4cebb17bd8a1fc7f528f48aef6045f4e5b0bb9e2
Paul de Mast <pjf.demast@avans.nl>
2021-11-17 13:59:34 +0100

    implement binding in MovieViewHolder

--- app/src/main/java/com/avans/avd/lesrecyclerview/adapter/MovieAdapter.kt
+++ app/src/main/java/com/avans/avd/lesrecyclerview/adapter/MovieAdapter.kt
@@ -3,7 +3,12 @@ package com.avans.avd.lesrecyclerview.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.avans.avd.lesrecyclerview.R
+import com.avans.avd.lesrecyclerview.model.MovieUiModel
 
 class MovieAdapter(
     private val layoutInflater: LayoutInflater,
@@ -11,7 +16,17 @@ class MovieAdapter(
 
     inner class MovieViewHolder(
         containerView: View,
-    ) : RecyclerView.ViewHolder(containerView)
+    ) : RecyclerView.ViewHolder(containerView) {
+        val movieTitle:TextView = containerView.findViewById(R.id.item_movie_title)
+        val movieType:TextView = containerView.findViewById(R.id.item_movie_type)
+        val moviePoster: ImageView = containerView.findViewById(R.id.item_movie_image)
+
+        fun bindData(movieData: MovieUiModel){
+            movieTitle.text = "${movieData.title} (${movieData.year})"
+            movieType.text = movieData.type
+            moviePoster.load(movieData.poster)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         TODO("Not yet implemented")
